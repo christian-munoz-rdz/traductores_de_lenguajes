21eb16f0c5c7d5fa5272e4e57aa3857b046e87fe
Christian_Windows_Desktop <chgeovany.15@gmail.com>
2023-03-01 01:06:26 -0600

    Act 5 SSP

--- ssp/Act5.asm
+++ ssp/Act5.asm
@@ -1,251 +1,382 @@
 org 100h 
 jmp inicio
-u1 db 0
-d1 db 0
-c1 db 0
-m1 db 0
-n1 dw 0 
-u2 db 0
-d2 db 0
-c2 db 0
-m2 db 0
-n2 dw 0
-u3 db 0
-d3 db 0
-c3 db 0
-m3 db 0
-ms3 db 0
-n3 dw 0
-msg db "Sumador/restador de 4 digitos$"
-prompt1 db "Suma o resta: $"
-prompt2 db "Ingrese su numero: $"
-sumares db "El resultado de la suma es: $"
-restares db "El resultado de la resta es: $"
-choice db ?
 
+;variables para el primer numero
+unid1 db 0
+de1 db 0
+cen1 db 0
+mill1 db 0
+num1 dw 0
+
+;variables para el segundo numero
+unid2 db 0
+de2 db 0
+cen2 db 0
+mill2 db 0
+num2 dw 0
+
+;variables para el resultado
+unid3 db 0
+de3 db 0
+cen3 db 0
+mill3 db 0
+dcm db 0
+num3 dw 0
+
+;Mensajes de pantalla
+msg db "Sumador y restador $"
+msg2 db "Elija una opcion (s = suma / cualquier otro = resta): $"
+msg3 db "Ingrese un numero: $"
+msg4 db "El resultado es: $"
+
+; Programa principal
 inicio:
-mov dx, offset msg
-mov ah, 09h
-int 21h
-call newline
+    LEA DX, msg 
+    MOV ah, 09h
+    INT 21h
+    MOV al, 10
+    MOV ah,0eh  
+    INT 10h
 
-mov dx, offset prompt1
-mov ah, 09h
-int 21h 
+    MOV al, 13
+    MOV ah,0eh  
+    INT 10h
 
-mov ah, 01h
-int 21h
-cmp al, 's'
-jz suma
-jmp resta
-          
-ret
+    LEA DX, msg2
+    MOV ah, 09h
+    INT 21h 
+
+    MOV ah, 01h
+    INT 21h
+    CMP al, 's'
+    jz suma
+    jmp resta
+            
+    ret
+
 
 suma:
-call newline
-call obt1
-call newline
-call obt2  
+    MOV al, 10
+    MOV ah,0eh  
+    INT 10h
+
+    MOV al, 13
+    MOV ah,0eh  
+    INT 10h
+    LEA DX, msg3
+    MOV ah, 09h
+    INT 21h
+
+    MOV ah, 01h
+    INT 21h
+    SUB al, 30h
+    MOV mill1, al ;Obtengo el digito de los millares del primer numero
+    MOV ah, 01h
+    INT 21h
+    SUB al, 30h
+    MOV cen1, al ;Obtengo el digito de las centenas del primer numero
+
+    MOV ah, 01h
+    INT 21h
+    SUB al, 30h
+    MOV de1, al ;Obtengo el digito de las decenas del primer numero   
+
+    MOV ah, 01h
+    INT 21h
+    SUB al, 30h
+    MOV unid1, al ;Obtengo el digito de las unidades del primer numero    
+
+    XOR ax, ax 
+
+    MOV bx, 1000 ;Multiplico el digito de los millares por 1000
+    MOV al, mill1  
+    MUL bx      
+    MOV num1, ax ;Guardo el resultado en num1 
+
+    XOR bx, bx ;Limpio el registro bx  
+
+    MOV bl, 100 ;Multiplico el digito de las centenas por 100 
+    MOV al, cen1 
+    MUL bl      
+    ADD num1, ax  ;Sumo el resultado a num1
+
+    MOV bl, 10 ;Multiplico el digito de las decenas por 10
+    MOV al, de1
+    MUL bl
+    ADD num1, ax  ;Sumo el resultado a num1
+
+    MOV bl, unid1 ;Sumo el digito de las unidades a num1  
+    ADD num1, bx   ;Sumo el resultado a num1
+    MOV al, 10 
+    MOV ah,0eh  
+    INT 10h 
+
+    MOV al, 13 
+    MOV ah,0eh  
+    INT 10h
+    LEA DX, msg3
+    MOV ah, 09h
+    INT 21h
+
+    MOV ah, 01h ;Obtengo el digito de los millares del segundo numero
+    INT 21h 
+    SUB al, 30h ;Convierto el caracter a numero
+    MOV mill2, al  ;Guardo el digito en mill2
+
+    MOV ah, 01h ;Obtengo el digito de las centenas del segundo numero
+    INT 21h 
+    SUB al, 30h ;Convierto el caracter a numero
+    MOV cen2, al  ;Guardo el digito en cen2
+
+    MOV ah, 01h ;Obtengo el digito de las decenas del segundo numero
+    INT 21h
+    SUB al, 30h ;Convierto el caracter a numero
+    MOV de2, al  ;Guardo el digito en de2
+
+    MOV ah, 01h ;Obtengo el digito de las unidades del segundo numero
+    INT 21h
+    SUB al, 30h 
+    MOV unid2, al ;Guardo el digito en unid2   
+
+    XOR ax, ax 
+
+    MOV bx, 1000 ;Multiplico el digito de los millares por 1000
+    MOV al, mill2  ;Obtengo el digito de los millares del segundo numero
+    MUL bx      
+    MOV num2, ax ;Guardo el resultado en num2
+
+    XOR bx, bx ;Limpio el registro bx  
+
+    MOV bl, 100 ;Multiplico el digito de las centenas por 100
+    MOV al, cen2 ;Obtengo el digito de las centenas del segundo numero
+    MUL bl      ;Multiplico el digito de las centenas por 100
+    ADD num2, ax ;Sumo el resultado a num2
+
+    MOV bl, 10 ;Multiplico el digito de las decenas por 10
+    MOV al, de2 ;Obtengo el digito de las decenas del segundo numero
+    MUL bl ;Multiplico el digito de las decenas por 10
+    ADD num2, ax  ;Sumo el resultado a num2
+
+    MOV bl, unid2  ;Obtengo el digito de las unidades del segundo numero
+    ADD num2, bx  ;Sumo el resultado a num2
 
 
-mov al, u1
-add al, u2
-aaa
-mov u3, al
+    MOV al, unid1 ;Sumo el digito de las unidades del primer numero
+    ADD al, unid2 ;Sumo el digito de las unidades del segundo numero
+    aaa 
+    MOV unid3, al ;Guardo el resultado en unid3
 
-mov al, d1
-adc al, d2
-aaa
-mov d3, al
+    MOV al, de1 ;Sumo el digito de las decenas del primer numero
+    ADC al, de2 ;Sumo el digito de las decenas del segundo numero
+    aaa 
+    MOV de3, al ;Guardo el resultado en de3
 
-mov al, c1
-adc al, c2
-aaa
-mov c3, al
+    MOV al, cen1 ;Sumo el digito de las centenas del primer numero
+    ADC al, cen2 ;Sumo el digito de las centenas del segundo numero
+    aaa
+    MOV cen3, al ;Guardo el resultado en cen3
 
-mov al, m1
-adc al, m2
-aaa
-mov m3, al
+    MOV al, mill1 ;Sumo el digito de los millares del primer numero
+    ADC al, mill2 ;Sumo el digito de los millares del segundo numero
+    aaa
+    MOV mill3, al ;Guardo el resultado en mill3
 
-mov al, 0
-adc al, 0
-aaa
-mov ms3, al
+    MOV al, 0 
+    ADC al, 0
+    aaa
+    MOV dcm, al ;Guardo el resultado en dcm
 
-call newline
-mov dx, offset sumares
-mov ah, 09h
-int 21h      
-call displayN3
+    MOV al, 10 
+    MOV ah,0eh  
+    INT 10h
 
-ret
+    ;imprimo el resultado
+    MOV al, 13 
+    MOV ah,0eh  
+    INT 10h 
+    LEA DX, msg4
+    MOV ah, 09h
+    INT 21h      
+    ADD unid3, 30h ;Convierto el numero a caracter 
+    ADD de3, 30h ;Convierto el numero a caracter
+    ADD cen3, 30h ;Convierto el numero a caracter
+    ADD mill3, 30h ;Convierto el numero a caracter
+    ADD dcm, 30h ;Convierto el numero a caracter
+
+    MOV ah, 02h 
+                
+    MOV dl, dcm ;Imprimo el digito de las decenas de millar
+    INT 21h           
+    MOV dl, mill3 ;Imprimo el digito de los millares
+    INT 21h 
+    MOV dl, cen3 ;Imprimo el digito de las centenas
+    INT 21h
+    MOV dl, de3 ;Imprimo el digito de las decenas
+    INT 21h
+    MOV dl, unid3 ;Imprimo el digito de las unidades
+    INT 21h
+
+    ret
 
 resta:
-call newline
-call obt1
-call newline
-call obt2 
+    MOV al, 10
+    MOV ah,0eh  
+    INT 10h
 
-mov al, u1
-sbb al, u2
-mov u3, al
+    MOV al, 13
+    MOV ah,0eh  
+    INT 10h
+    LEA DX, msg3
+    MOV ah, 09h
+    INT 21h
 
-mov al, d1
-sbb al, d2
-mov d3, al
+    MOV ah, 01h
+    INT 21h
+    SUB al, 30h
+    MOV mill1, al ;Obtengo el digito de los millares del primer numero  
 
-mov al, c1
-sbb al, c2
-mov c3, al  
+    MOV ah, 01h
+    INT 21h
+    SUB al, 30h
+    MOV cen1, al ;Obtengo el digito de las centenas del primer numero  
 
-mov al, m1
-sbb al, m2
-mov m3, al
+    MOV ah, 01h
+    INT 21h
+    SUB al, 30h
+    MOV de1, al ;Obtengo el digito de las decenas del primer numero  
 
-mov al, 0
-sbb al, 0
-mov ms3, al
+    MOV ah, 01h
+    INT 21h
+    SUB al, 30h
+    MOV unid1, al ;Obtengo el digito de las unidades del primer numero ;Obtengo el digito de las unidades del primer numero   
 
-call newline
-mov dx, offset restares
-mov ah, 09h
-int 21h      
-call displayN3
+    XOR ax, ax 
 
-ret
+    MOV bx, 1000 ;Multiplico el digito de los millares por 1000
+    MOV al, mill1  
+    MUL bx      
+    MOV num1, ax ;Guardo el resultado en num1
 
-obt1:
-mov dx, offset prompt2
-mov ah, 09h
-int 21h
+    XOR bx, bx ;Limpio el registro bx  
 
-mov ah, 01h
-int 21h
-sub al, 30h
-mov m1, al  ;capta las milesimas de n1
+    MOV bl, 100 ;Multiplico el digito de las centenas por 100
+    MOV al, cen1 
+    MUL bl      
+    ADD num1, ax ;Sumo el resultado a num1
 
-mov ah, 01h
-int 21h
-sub al, 30h
-mov c1, al  ;capta las centenas de n1
+    MOV bl, 10 ;Multiplico el digito de las decenas por 10
+    MOV al, de1 
+    MUL bl
+    ADD num1, ax  ;Sumo el resultado a num1 
 
-mov ah, 01h
-int 21h
-sub al, 30h
-mov d1, al  ;capta las decenas del n1
+    MOV bl, unid1 ;Sumo el digito de las unidades a num1  
+    ADD num1, bx ;Sumo el digito de las unidades a num1 
+    MOV al, 10 
+    MOV ah,0eh  
+    INT 10h
 
-mov ah, 01h
-int 21h
-sub al, 30h
-mov u1, al  ;capta las unidades del n1 
+    MOV al, 13
+    MOV ah,0eh  
+    INT 10h
+    LEA DX, msg3
+    MOV ah, 09h
+    INT 21h
 
-xor ax, ax 
+    MOV ah, 01h
+    INT 21h
+    SUB al, 30h
+    MOV mill2, al ;Obtengo el digito de los millares del segundo numero  
 
-mov bx, 1000
-mov al, m1  ; m1 * 10000
-mul bx      ; bx porque es 16 bits y cubre a 1000
-mov n1, ax 
+    MOV ah, 01h 
+    INT 21h
+    SUB al, 30h
+    MOV cen2, al ;Obtengo el digito de las centenas del segundo numero  
 
-xor bx, bx  ; limpiamos todo bx, para no dejar nada en bx
+    MOV ah, 01h
+    INT 21h
+    SUB al, 30h
+    MOV de2, al  ;Obtengo el digito de las decenas del segundo numero
 
-mov bl, 100
-mov al, c1
-mul bl      ; c1 * 100
-add n1, ax
+    MOV ah, 01h
+    INT 21h
+    SUB al, 30h
+    MOV unid2, al  ;Obtengo el digito de las unidades del segundo numero
 
-mov bl, 10
-mov al, d1
-mul bl
-add n1, ax  ;c1 * 100 + dl * 10
+    XOR ax, ax 
 
-mov bl, u1  ; n1 = c1 * 100 + d1 * 10 + u1
-add n1, bx  ; guardamos el numero completo en n1                
+    MOV bx, 1000 ;Multiplico el digito de los millares por 1000
+    MOV al, mill2 
+    MUL bx      
+    MOV num2, ax ;Guardo el resultado en num2
 
-ret
+    XOR bx, bx ;Limpio el registro bx  
 
-obt2:
-mov dx, offset prompt2
-mov ah, 09h
-int 21h
+    MOV bl, 100 ;Multiplico el digito de las centenas por 100
+    MOV al, cen2
+    MUL bl      
+    ADD num2, ax ;Sumo el resultado a num2
 
-mov ah, 01h
-int 21h
-sub al, 30h
-mov m2, al  ;capta las milesimas de n1
+    MOV bl, 10 ;Multiplico el digito de las decenas por 10
+    MOV al, de2
+    MUL bl 
+    ADD num2, ax ;Sumo el resultado a num2  
 
-mov ah, 01h
-int 21h
-sub al, 30h
-mov c2, al  ;capta las centenas de n1
+    MOV bl, unid2  ;Sumo el digito de las unidades a num2
+    ADD num2, bx  
 
-mov ah, 01h
-int 21h
-sub al, 30h
-mov d2, al  ;capta las decenas del n1
+    MOV al, unid1 ;Resto el digito de las unidades del primer numero
+    SBB al, unid2 
+    MOV unid3, al
 
-mov ah, 01h
-int 21h
-sub al, 30h
-mov u2, al  ;capta las unidades del n1 
+    MOV al, de1 ;Resto el digito de las decenas del primer numero
+    SBB al, de2
+    MOV de3, al
 
-xor ax, ax 
+    MOV al, cen1 ;Resto el digito de las centenas del primer numero
+    SBB al, cen2
+    MOV cen3, al  
 
-mov bx, 1000
-mov al, m2  ; m1 * 10000
-mul bx      ; bx porque es 16 bits y cubre a 1000
-mov n2, ax 
+    MOV al, mill1 ;Resto el digito de los millares del primer numero
+    SBB al, mill2
+    MOV mill3, al
 
-xor bx, bx  ; limpiamos todo bx, para no dejar nada en bx
+    MOV al, 0  ;Resto el digito de las decenas de millar del primer numero
+    SBB al, 0
+    MOV dcm, al ;Guardo el resultado en dcm
 
-mov bl, 100
-mov al, c2
-mul bl      ; c1 * 100
-add n2, ax
+    ;Imprimo el resultado
+    MOV al, 10 
+    MOV ah,0eh  
+    INT 10h
 
-mov bl, 10
-mov al, d2
-mul bl
-add n2, ax  ;c1 * 100 + dl * 10
+    MOV al, 13
+    MOV ah,0eh  
+    INT 10h
+    LEA DX, msg4
+    MOV ah, 09h
+    INT 21h
+    ;Convierto los numeros a caracter      
+    ADD unid3, 30h
+    ADD de3, 30h
+    ADD cen3, 30h
+    ADD mill3, 30h
+    ADD dcm, 30h
 
-mov bl, u2  ; n1 = c1 * 100 + d1 * 10 + u1
-add n2, bx  ; guardamos el numero completo en n1                
+    MOV ah, 02h
+                
+    ;Imprimo el resultado
+    MOV dl, dcm
+    INT 21h           
+    MOV dl, mill3
+    INT 21h 
+    MOV dl, cen3
+    INT 21h
+    MOV dl, de3
+    INT 21h
+    MOV dl, unid3
+    INT 21h
 
-ret
-
-
-newline:
-mov al, 10
-mov ah,0eh  
-int 10h
-
-mov al, 13
-mov ah,0eh  
-int 10h
-ret   
-
-displayN3:
-add u3, 30h
-add d3, 30h
-add c3, 30h
-add m3, 30h
-add ms3, 30h
-
-mov ah, 02h
-            
-mov dl, ms3
-int 21h           
-mov dl, m3
-int 21h 
-mov dl, c3
-int 21h
-mov dl, d3
-int 21h
-mov dl, u3
-int 21h
- 
-ret
-
+    ret
 
 
 exit:
