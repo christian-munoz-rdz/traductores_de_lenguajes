ce4bba1de79d61dc93f1850aadb83a0583349497
Christian_Windows_Desktop <chgeovany.15@gmail.com>
2023-03-03 11:47:34 -0600

    Version final Act. 6

--- ssp/Act6.asm
+++ ssp/Act6.asm
@@ -22,22 +22,22 @@ inicio:
   MOV ah, 01h
   INT 21h
   SUB al, 30h
-  MOV m1, al  ;capta las milesimas de n1
+  MOV m1, al  ;Obtenemos el digito de las unidades de millar de n1
     
   MOV ah, 01h
   INT 21h
   SUB al, 30h
-  MOV c1, al  ;capta las centenas de n1
+  MOV c1, al  ; Obtenemos el digito de las centenas de n1
     
   MOV ah, 01h
   INT 21h
   SUB al, 30h
-  MOV d1, al  ;capta las decenas del n1
+  MOV d1, al  ; Obtenemos el digito de las decenas de n1
     
   MOV ah, 01h
   INT 21h
   SUB al, 30h
-  MOV u1, al  ;capta las unidades del n1 
+  MOV u1, al  ; Obtenemos el digito de las unidades de n1
     
   XOR ax, ax 
     
@@ -51,88 +51,90 @@ inicio:
   MOV bl, 100
   MOV al, c1
   MUL bl      ; c1 * 100
-  ADD n1, ax
+  ADD n1, ax ; sumamos las centenas a n1
     
   MOV bl, 10
-  MOV al, d1
-  MUL bl
-  ADD n1, ax  ;c1 * 100 + dl * 10
+  MOV al, d1 ;c1 * 100 + dl * 10
+  MUL bl 
+  ADD n1, ax  ;Sumamos las decenas a n1
     
   MOV bl, u1  ; n1 = c1 * 100 + d1 * 10 + u1
   ADD n1, bx  ; guardamos el numero completo en n1                
     
   call newline
-  MOV dx, offset msg1
+  MOV dx, offset msg1 ; pedimos el segundo numero
   MOV ah, 09h
   INT 21h
     
   MOV ah, 01h
   INT 21h
   SUB al, 30h
-  MOV m2, al  ;capta las milesimas de n1
+  MOV m2, al  ;capta las unidades de millar de n2
     
   MOV ah, 01h
   INT 21h
   SUB al, 30h
-  MOV c2, al  ;capta las centenas de n1
+  MOV c2, al  ;capta las centenas de n2
     
   MOV ah, 01h
   INT 21h
   SUB al, 30h
-  MOV d2, al  ;capta las decenas del n1
+  MOV d2, al  ;capta las decenas del n2
     
   MOV ah, 01h
   INT 21h
   SUB al, 30h
-  MOV u2, al  ;capta las unidades del n1 
+  MOV u2, al  ;capta las unidades del n2 
     
   XOR ax, ax 
     
   MOV bx, 1000
-  MOV al, m2  ; m1 * 10000
+  MOV al, m2  ; m1 * 1000
   MUL bx      ; bx porque es 16 bits y cubre a 1000
-  MOV n2, ax 
+  MOV n2, ax ;guardamos las unidades de millar en n2
     
   XOR bx, bx  ; limpiamos todo bx, para no dejar nada en bx
     
   MOV bl, 100
   MOV al, c2
   MUL bl      ; c1 * 100
-  ADD n2, ax
+  ADD n2, ax ; sumamos las centenas a n2
     
   MOV bl, 10
   MOV al, d2
   MUL bl
-  ADD n2, ax  ;c1 * 100 + dl * 10
+  ADD n2, ax  ;Sumamos las decenas a n2
   call newline
     
   MOV bl, u2  ; n1 = c1 * 100 + d1 * 10 + u1
-  ADD n2, bx  ; guardamos el numero completo en n1 
+  ADD n2, bx  ; guardamos el numero completo en n2 
 
-  MOV dx, offset msg2
+  MOV dx, offset msg2 ; Imprimimos el mensaje de resultado
   MOV ah, 09h
   INT 21h
 
-  MOV AX, n1 ; multiplicando n�mero 1
-  MOV BX, n2 ; multiplicando n�mero 2
+  MOV AX, n1 ; multiplicando numero 1
+  MOV BX, n2 ; multiplicando numero 2
   MOV CX, 0000H ; contador inicializado a 0
   MOV DX, 0000H ; inicializar el registro de la suma a 0
 
   LOOP_START:
-    TEST BX, 0001H ; comprobar si el �ltimo bit de multiplicando n�mero 2 est� establecido
-    JZ SKIP_ADDITION ; saltar si el �ltimo bit est� en 0
-    ADD DX, AX ; sumar multiplicando n�mero 1 al registro de la suma
+    TEST BX, 0001H ; comprobar si el ultimo bit de multiplicando numero 2 esta establecido
+    JZ SKIP_ADDITION ; saltar si el ultimo bit esta en 0
+    ADD DX, AX ; sumar multiplicando numero 1 al registro de la suma
   SKIP_ADDITION:
-    SHL AX, 1 ; multiplicando n�mero 1 se desplaza un bit a la izquierda
-    SHR BX, 1 ; multiplicando n�mero 2 se desplaza un bit a la derecha
+    SHL AX, 1 ; multiplicando numero 1 se desplaza un bit a la izquierda
+    SHR BX, 1 ; multiplicando numero 2 se desplaza un bit a la derecha
     INC CX ; incrementar el contador
     CMP CX, 16 ; comprobar si se han realizado 16 iteraciones
     JL LOOP_START ; saltar al inicio del bucle si no se han realizado todas las iteraciones
 
   MOV result, DX ; almacenar el resultado en la variable result
+
+  ; Imprimir el resultado
   
-  ; Imprimir el primer d�gito
-  MOV BX, 10000
+  ; Imprimir el primer digito
+  MOV BX, 10000 
   MOV AX, result
   XOR DX, DX
   DIV BX
@@ -144,7 +146,7 @@ inicio:
   MOV DL, AL
   INT 21h
     
-  ; Imprimir el segundo d�gito
+  ; Imprimir el segundo digito
   MOV BX, 1000
   MOV AX, result
   XOR DX, DX
@@ -157,7 +159,7 @@ inicio:
   MOV DL, AL
   INT 21h
     
-  ; Imprimir el tercer d�gito
+  ; Imprimir el tercer digito
   MOV BX, 100
   MOV AX, result
   XOR DX, DX
@@ -170,7 +172,7 @@ inicio:
   MOV DL, AL
   INT 21h
     
-  ; Imprimir el cuarto d�gito
+  ; Imprimir el cuarto digito
   MOV BX, 10
   MOV AX, result
   XOR DX, DX
@@ -183,7 +185,7 @@ inicio:
   MOV DL, AL
   INT 21h
     
-  ; Imprimir el quinto d�gito
+  ; Imprimir el quinto digito
   MOV BX, 1
   MOV AX, result
   XOR DX, DX
