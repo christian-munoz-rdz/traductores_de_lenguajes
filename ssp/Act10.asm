104109fdd993ad156767ab9394876b564801bb5d
Christian_Windows_Desktop <chgeovany.15@gmail.com>
2023-05-09 22:51:26 -0600

    Primera versión de la actividad 10

--- ssp/Act10.asm
+++ ssp/Act10.asm
@@ -1,37 +1,92 @@
-MOV AH, 0
-MOV AL, 13H
-INT 10H
+org 100h   
 
-MOV AH, 0CH
-MOV AL, 10
-MOV CX, 30
-MOV DX, 50
-INT 10H
+jmp start 
 
-MOV BL, 70
-firstline:
-    INT 10h
-    INC CX
-    DEC BL
-    JNZ firstline
-    MOV BL,70
+corX0 dw ?           
+corY0 dw ?            
 
-secondline:
-    INT 10H
-    INC CX
-    DEC BL
-    JNZ secondline
-    MOV BL, 70
+corX1 dw ?           
+corY1 dw ?              
+
+
+COLOR EQU 0Fh       
+                    
+macro DRAW             
+    mov ah,0Ch            
+    mov al, COLOR   
+    int 10h
+    endm
+
+start:
+    mov ax, @data   
+    mov ds, ax      
+     
+    mov ah, 0       
+    mov al, 13h     
+    int 10h     
     
-thirdline:
-    INT 10H
-    DEC CX
-    DEC BL
-    JNZ thirdline
-    MOV BL, 70
+    mov ah,0h       
+    int 33h            
+    
+repite:    
+    mov ax, 03h     
+    int 33h
+     
+    mov corX0, cx   
+    mov corY0, dx
+    
+    cmp bx, 1           
+    mov bx, 0
+    jne repite       
 
-fourthline:
-    INT 10h
-    DEC DX
-    DEC BL
-    JNZ fourthline
+repite1: 
+    mov ax,03h      
+    int 33h
+     
+    mov corX1, cx   
+    mov corY1, dx
+    
+    cmp bx, 1       
+    jne repite1      
+    
+    mov ah,0Ch            
+    mov al, COLOR   
+  
+    mov cx, corX0   
+    mov dx, corY0
+    
+dibujaX0:
+  
+    int 10h             
+    inc cx           
+    cmp cx, corX1    
+    jne dibujaX0        
+
+dibujaY1: 
+   
+    int 10h             
+    inc dx           
+    cmp dx, corY1    
+    jne dibujaY1     
+ 
+dibujaX1:
+  
+    int 10h             
+    dec cx           
+    cmp cx, corX0    
+    jne dibujaX1       
+
+dibujaY0:  
+  
+    int 10h             
+    dec dx           
+    cmp dx, corY0    
+    jne dibujaY0     
+        
+    mov ah, 0h      
+    int 16h
+    
+ret
+                 
+                 
+                 
