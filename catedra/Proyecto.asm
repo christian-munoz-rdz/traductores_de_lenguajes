15272f1db5a1da67647c49e161eaef1c6b8210c9
Christian_Windows_Desktop <chgeovany.15@gmail.com>
2023-05-18 00:57:13 -0600

    Proyecto final terminado

--- catedra/Proyecto.asm
+++ catedra/Proyecto.asm
@@ -1,169 +1,115 @@
-.model small
-.stack 100h
+.MODEL SMALL
+.STACK 100h
 
-.data
-    x1 dw 10  ; Punto de inicio (x1, y1)
-    y1 dw 10
-    x2 dw 30  ; Punto final (x2, y2)
-    y2 dw 20
-    
-.code
-    mov ax, @data
-    mov ds, ax
-    
-    ; Inicializar el modo de vídeo
-    mov ax, 13h
-    int 10h
-    
-    ; Dibujar la línea
-    mov ax, x1
-    mov bx, y1
-    mov cx, x2
-    mov dx, y2
-    call Bresenham
-    
-    ; Salir del programa
-    mov ax, 4c00h
-    int 21h
-    
-Bresenham proc
-    push ax
-    push bx
-    push cx
-    push dx
-    
-    mov ax, 0A000h   ; Segmento de video en modo gráfico 13h
-    mov es, ax
-    
-    mov ax, cx
-    sub ax, bx       ; dx = x2 - x1
-    mov dx, ax
-    jns @skip1       ; Saltar si dx >= 0
-    
-    neg dx           ; dx = -dx
-    neg ax           ; Invertir x1
-    
-@skip1:
-    mov bx, dx       ; bx = |dx|
-    
-    mov ax, dx
-    mov cx, ax       ; cx = |dx|
-    shl cx, 1        ; cx = 2 * |dx|
-    shl ax, 2        ; ax = 4 * |dx|
-    add ax, cx       ; ax = 5 * |dx|
-    
-    mov cx, ax       ; cx = 5 * |dx|
-    shr cx, 1        ; cx = (5 * |dx|) / 2
-    mov dx, 0        ; dy = 0
-    
-    mov ax, x1
-    mov bx, y1
-    mov di, bx       ; di = y1
-    
-    mov ax, x2
-    cmp ax, x1
-    jl @skip2        ; Saltar si x2 < x1
-    
-    mov ax, 1        ; Incremento positivo para x
-    mov si, x2       ; si = x2
-    
-    jmp @skip3
-    
-@skip2:
-    mov dx, ax
-    sub ax, y2       ; dx = y1 - y2
-    jns @skip3       ; Saltar si dx >= 0
-    
-    neg dx           ; dx = -dx
-    neg ax           ; Invertir y1
-    
-@skip3:
-    mov bx, dx       ; bx = |dx|
-    
-    mov ax, dx
-    mov cx, ax       ; cx = |dx|
-    shl cx, 1        ; cx = 2 * |dx|
-    shl ax, 2        ; ax = 4 * |dx|
-    add ax, cx       ; ax = 5 * |dx|
-    
-    mov cx, ax       ; cx = 5 * |dx|
-    shr cx, 1        ; cx = (5 * |dx|) / 2
-    mov dx, 0        ; dy
-    mov ax, y1
-    
-@loop:
-    push ax          ; Guardar el valor actual de y
-    
-    mov ax, bx
-    cmp ax, cx
-    jge @skip4      ; Saltar si bx >= cx
-    
-    mov ax, 1       ; Incremento positivo para x
-    
-@skip4:
-    mov dx, ax
-    sub ax, y2       ; dx = y1 - y2
-    jns @skip5       ; Saltar si dx >= 0
-    
-    neg dx           ; dx = -dx
-    neg ax           ; Invertir y1
-    
-@skip5:
-    mov bx, dx       ; bx = |dx|
-    
-    mov ax, dx
-    mov cx, ax       ; cx = |dx|
-    shl cx, 1        ; cx = 2 * |dx|
-    shl ax, 2        ; ax = 4 * |dx|
-    add ax, cx       ; ax = 5 * |dx|
-    
-    mov cx, ax       ; cx = 5 * |dx|
-    shr cx, 1        ; cx = (5 * |dx|) / 2
-    mov dx, 0        ; dy = 0
-    
-    mov ax, y1
-    cmp ax, y2
-    jge @skip6       ; Saltar si y1 >= y2
-    
-    mov ax, 1        ; Incremento positivo para y
-    
-@skip6:
-    add di, ax       ; Incrementar di (posición de y)
-    sub cx, bx       ; cx = cx - bx
-    
-    jnz @skip7       ; Saltar si cx != 0
-    
-    add ax, dx       ; Incrementar y (dy = dy + 1)
-    mov cx, ax       ; cx = dy
-    
-@skip7:
-    mov ax, x1
-    mov bx, x2
-    cmp ax, bx
-    je @skip8        ; Saltar si x1 == x2
-    
-    mov ax, x1
-    mov bx, 320      ; Ancho de la pantalla
-    
-    mul bx           ; ax = x1 * 320
-    
-    add ax, di       ; ax = x1 * 320 + di
-    
-    mov bx, ax       ; bx = dirección de memoria
-    
-    mov word ptr es:[bx], 15h  ; Dibujar el píxel
-    
-    mov ax, 1        ; Incremento positivo para x
-    add x1, ax
-    jmp @skip7
-    
-@skip8:
-    pop dx
-    pop cx
-    pop bx
-    pop ax
-    
-    ret
-Bresenham endp
+.DATA
+    x1 DW 160
+    y1 DW 100
+    x2 DW 320
+    y2 DW 200
+    dxs DW ?
+    dy DW ?
+    E DW ?                                              
+    Dn DW ?
+    Dp DW ?
+    x DW ?
+    y DW ?
 
-end
+.CODE
+    ASSUME DS:DATA, CS:CODE
+
+BEGIN:
+    MOV AX, @DATA
+    MOV DS, AX
+
+    CALL GRAPHMODE
+    CALL BRESENHAM
+    CALL LINEA
+    CALL GETKEY
+
+    MOV AX, 4C00h
+    INT 21h
+
+GRAPHMODE PROC NEAR
+    MOV AX, 0
+    MOV AL, 13h
+    INT 10h
+    RET
+
+GETKEY PROC NEAR
+    MOV AH, 10h
+    INT 16h
+    RET
+
+BRESENHAM PROC NEAR
+    ; Calcula las diferencias en las coordenadas x e y
+    MOV AX, x2
+    SUB AX, x1
+    MOV DX, AX
+    MOV dxs, DX
+    MOV AX, y2
+    SUB AX, y1
+    MOV DX, AX
+    MOV dy, DX
+
+    ; Calcula E, Dn y Dp
+    MOV AX, 2
+    MUL dy
+    SUB AX, dxs
+    MOV E, AX
+    MOV AX, 2
+    MUL dy
+    MOV DX, AX
+    SUB DX, dxs
+    MOV Dp, DX
+    MOV AX, 2
+    MUL dy
+    MOV Dn, AX
+
+    ; Inicializa las coordenadas x, y
+    MOV AX, x1
+    MOV x, AX
+    MOV AX, y1
+    MOV y, AX
+
+    RET
+
+LINEA PROC NEAR
+    MOV CX, dxs
+
+    ; Calcula el desplazamiento horizontal para centrar la línea
+    MOV AX, 320
+    SUB AX, dxs
+    SHR AX, 1
+    MOV DX, AX
+    ADD x, DX
+
+    ; Calcula el desplazamiento vertical para centrar la línea
+    MOV AX, 200
+    SUB AX, dy
+    SHR AX, 1
+    MOV DX, AX
+    ADD y, DX
+
+    DRAW_LINE:
+        MOV AH, 0Ch
+        INT 10h
+
+        MOV AX, E
+        CMP AX, 0
+        JL ELSE_CONDITION
+
+        ADD AX, Dp
+        ADD x, 1
+        MOV E, AX
+        JMP CONTINUE_LOOP
+
+    ELSE_CONDITION:
+        ADD AX, Dn
+        MOV E, AX
+
+    CONTINUE_LOOP:
+        LOOP DRAW_LINE
+
+    RET
+
+END BEGIN
